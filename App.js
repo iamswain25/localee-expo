48002edc4036c0f6b4ed15203933dc153a11f70f
swain <iamswain@hotmail.com>
2019-02-19 18:52:28 -0800

    search draggable

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Animated } from "react-native";
+import { MapView, Constants, GestureHandler } from "expo";
+import BottomSearch from "./components/BottomSearch";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this._translateY = new Animated.Value(0);
+  }
+  _onHandlerStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      this._lastOffset.x += event.nativeEvent.translationX;
+      this._lastOffset.y += event.nativeEvent.translationY;
+      this._translateX.setOffset(this._lastOffset.x);
+      this._translateX.setValue(0);
+      this._translateY.setOffset(this._lastOffset.y);
+      this._translateY.setValue(0);
+    }
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MapView
+          style={{ alignSelf: "stretch", height: 200, flex: 1 }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        >
+          <MapView.Marker
+            coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+            title="ok"
+            description="desc"
+          >
+            <View>
+              <Text>#OK!</Text>
+            </View>
+          </MapView.Marker>
+        </MapView>
+        <BottomSearch />
       </View>
     );
   }
@@ -13,9 +48,15 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  searchBar: {
+    position: "absolute",
+    bottom: 0,
+    height: "10%"
+  }
 });
---
+++ components/BottomSearch.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { StyleSheet, Text, View, Animated, Dimensions } from "react-native";
+import { GestureHandler } from "expo";
+
+const TOP_SHOW_HEIGHT = 600;
+const BOTTOM_SHOW_HEIGHT = 100;
+const BOTTOM_HIDDEN = 600;
+const styles = StyleSheet.create({
+  searchBar: {
+    width: "100%",
+    position: "absolute",
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    bottom: BOTTOM_SHOW_HEIGHT - TOP_SHOW_HEIGHT - BOTTOM_HIDDEN,
+    height: TOP_SHOW_HEIGHT + BOTTOM_HIDDEN
+  }
+});
+
+export default class BottomSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    // this._translateX = new Animated.Value(0);
+    this._translateY = new Animated.Value(0);
+    this._lastOffset = { y: 0 };
+    this._onGestureEvent = Animated.event([
+      {
+        nativeEvent: {
+          //   translationX: this._translateX,
+          translationY: this._translateY
+        }
+      }
+    ]);
+    // const {height} = Dimensions.get('window');
+  }
+  _onHandlerStateChange = event => {
+    if (event.nativeEvent.oldState === GestureHandler.State.ACTIVE) {
+      this._lastOffset.y += event.nativeEvent.translationY;
+      const y = this._lastOffset.y;
+      if (y > 0) {
+        console.log(y, "down");
+        this._translateY.setOffset(0);
+        this._lastOffset.y = 0;
+      } else if (y < BOTTOM_SHOW_HEIGHT - TOP_SHOW_HEIGHT) {
+        console.log(y, "up");
+        this._translateY.setOffset(BOTTOM_SHOW_HEIGHT - TOP_SHOW_HEIGHT);
+        this._lastOffset.y = BOTTOM_SHOW_HEIGHT - TOP_SHOW_HEIGHT;
+      } else {
+        console.log(y, "else");
+        this._translateY.setOffset(this._lastOffset.y);
+      }
+      this._translateY.setValue(0);
+    }
+  };
+
+  render() {
+    return (
+      <GestureHandler.PanGestureHandler
+        onGestureEvent={this._onGestureEvent}
+        onHandlerStateChange={this._onHandlerStateChange}
+      >
+        <Animated.View
+          style={[
+            styles.searchBar,
+            {
+              transform: [
+                // { translateX: this._translateX },
+                { translateY: this._translateY }
+              ]
+            }
+          ]}
+        >
+          <Text>-----------</Text>
+          <Text>input box</Text>
+        </Animated.View>
+      </GestureHandler.PanGestureHandler>
+    );
+  }
+}
